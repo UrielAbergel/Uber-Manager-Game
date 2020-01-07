cbe361151d1101629f4b13da6efb5cc99e63ccd1
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2020-01-07 20:52:42 +0200

    a

---
+++ src/gameClient/Fruits.java
@@ -0,0 +1,54 @@
+package gameClient;
+
+
+import dataStructure.*;
+import utils.Point3D;
+
+public class Fruits implements node_data {
+
+
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
